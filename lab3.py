079dfbaf5d7785baad7beb5874066f041494eec0
nhanda0 <159860532+nhanda0@users.noreply.github.com>
2024-02-12 22:40:38 -0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,3 +1,4 @@
-def helloWorld():
-      print ('Hello World' )
-helloWorld()
+def write_text_file():
+    with open("your_name.txt", "w") as file:
+        file.write("Your Name")
+    print("Text file created successfully.")
